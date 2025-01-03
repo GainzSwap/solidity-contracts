ede5c5acb48f7892ce7cb782ef3d081bc8e9a218
newtmex <brightomex@gmail.com>
2025-01-03 01:45:58 +0100

    feat: update runE2ELocalnet script to include axios for user stats retrieval and adjust account selection logic

--- scripts/runE2ELocalnet/index.ts
+++ scripts/runE2ELocalnet/index.ts
@@ -3,16 +3,25 @@ import { task } from "hardhat/config";
 import swap from "./swap";
 import stake from "./stake";
 import { randomNumber, sleep } from "../../utilities";
+import axios from "axios";
 
 task("runE2ELocalnet", "").setAction(async (_, hre) => {
   const actions = [swap, stake];
   const accounts = await hre.ethers.getSigners();
 
   while (true) {
-    await sleep(randomNumber(1000, 6_000));
     const accountStart = randomNumber(0, accounts.length);
     const accountEnd = randomNumber(accountStart, accounts.length);
+    const selectedAccounts = accounts.slice(accountStart, accountEnd);
 
-    await actions[randomNumber(0, actions.length)](hre, accounts.slice(accountStart, accountEnd));
+    await actions[randomNumber(0, actions.length)](hre, selectedAccounts);
+
+    await Promise.all(
+      selectedAccounts.map(account =>
+        axios.get("http://localhost:3000/api/user/stats/" + account.address + "?chainId=31337"),
+      ),
+    );
+    
+    await sleep(randomNumber(1000, 6_000));
   }
 });
