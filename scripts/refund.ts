64603c2cc733cc8c67324e7f28e8f2beb1d9c2b1
newtmex <brightomex@gmail.com>
2025-01-20 09:47:10 +0100

    final update for upgrade mainnet

--- scripts/refund.ts
+++ scripts/refund.ts
@@ -4,9 +4,9 @@ import { Router } from "../typechain-types";
 
 task("refund", "").setAction(async (_, hre) => {
   const { ethers } = hre;
-  const { deployer } = await hre.getNamedAccounts();
+  const { newOwner, newFeeTo } = await hre.getNamedAccounts();
 
-  const router = await ethers.getContract<Router>("Router", deployer);
+  const router = await ethers.getContract<Router>("Router", newOwner);
   const governanceAddress = await router.getGovernance();
   const governance = await ethers.getContractAt("Governance", governanceAddress);
 
@@ -15,5 +15,5 @@ task("refund", "").setAction(async (_, hre) => {
 
   const { fundsRaised } = await launchPair.getCampaignDetails(1);
 
-  await launchPair.refundCampaign(1, { value: fundsRaised });
+  await launchPair.connect(await hre.ethers.getSigner(newFeeTo)).refundCampaign(1, { value: fundsRaised });
 });
