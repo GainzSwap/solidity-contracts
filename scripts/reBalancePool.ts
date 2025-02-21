f4e93a6b179d404434b98d2fee2092dda6cfa0ca
newtmex <brightomex@gmail.com>
2025-02-21 07:36:04 +0100

    refactor reBalancePool script: enhance logic for token swapping and improve liquidity calculations

--- scripts/reBalancePool.ts
+++ scripts/reBalancePool.ts
@@ -1,16 +1,32 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { task } from "hardhat/config";
 import { Router } from "../typechain-types";
-import { formatEther, formatUnits, parseEther, parseUnits } from "ethers";
+import { formatEther, formatUnits, parseEther } from "ethers";
 
 task("reBalancePool").setAction(async (_, hre) => {
   const { ethers } = hre;
   const { deployer, newFeeTo } = await hre.getNamedAccounts();
   const router = await ethers.getContract<Router>("Router", deployer);
+  const governance = await ethers.getContractAt("Governance", await router.getGovernance());
+  const gToken = await ethers.getContractAt("GToken", await governance.getGToken());
+  const launchPair = await ethers.getContractAt("LaunchPair", await governance.launchPair());
 
-  const pair = await ethers.getContractAt("Pair", "0xE9DDDDAB75354Ab7ea2365e66902CA61930796C4");
+  const [pairAddr] = await router.pairs();
+
+  const pair = await ethers.getContractAt("Pair", pairAddr);
   const token0 = await ethers.getContractAt("ERC20", await pair.token0());
   const token1 = await ethers.getContractAt("ERC20", await pair.token1());
+
+  const gTokenSupply = await gToken.totalSupply();
+  const totalPairLiq = await pair.totalSupply();
+  const [userGTokenAmount, userPairLiq] = (await gToken.getGTokenBalance(deployer)).reduce(
+    ([gTokenTotal, liquidity], cur) => [gTokenTotal + cur.amount, liquidity + cur.attributes.lpDetails.liquidity],
+    [0n, 0n],
+  );
+
+  const wntv = await ethers.getContractAt("WNTV", await router.getWrappedNativeToken());
+  const { fundsRaised } = await launchPair.getCampaignDetails(1);
+  const totalDEDU = await wntv.totalSupply();
 
   // const { reserve0, reserve1 } = await pair.getReserves();
 
@@ -35,9 +51,17 @@ task("reBalancePool").setAction(async (_, hre) => {
     console.log({ usdcReserve: formatUnits(usdcReserve, 6), eduReserve: formatEther(eduReserve) });
     const ratio = (eduReserve / usdcReserve) * 10n ** 6n;
     console.log({ ratio: formatEther(ratio) });
-    if (ratio <= parseEther("3.33333333")) break;
 
-    const amountIn = (usdcReserve * 18n) / 10_000n;
+    console.log({
+      tvl: formatEther(eduReserve + fundsRaised + totalDEDU),
+      liqPercent: formatUnits((userPairLiq * 100_00n) / totalPairLiq, 2),
+      eduShare: formatEther((userPairLiq * eduReserve) / totalPairLiq),
+      usdcShare: formatUnits((userPairLiq * usdcReserve) / totalPairLiq, 6),
+    });
+
+    if (ratio <= parseEther("3.696360")) break;
+
+    const amountIn = (usdcReserve * 4n) / 10_000n;
     const amountOut = (eduReserve * amountIn) / usdcReserve;
 
     const { hash } = await router.swapExactTokensForTokens(
