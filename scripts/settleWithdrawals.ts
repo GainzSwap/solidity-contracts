95156f3dc233446b0d6ad882f7b2e0cbe1754842
newtmex <brightomex@gmail.com>
2025-02-22 07:10:32 +0100

    enhance settleWithdrawals script: add logging for pending withdrawals and handle zero withdrawal case

--- scripts/settleWithdrawals.ts
+++ scripts/settleWithdrawals.ts
@@ -1,6 +1,7 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { task } from "hardhat/config";
 import { Router } from "../typechain-types";
+import { formatEther } from "ethers";
 
 task("settleWithdrawals").setAction(async (_, hre) => {
   const { ethers } = hre;
@@ -12,6 +13,9 @@ task("settleWithdrawals").setAction(async (_, hre) => {
 
   const value = await wNative.pendingWithdrawals();
   const balance = await ethers.provider.getBalance(feeTo);
+
+  console.log({ pendingWithdrawal: formatEther(value) });
+  if (value == 0n) return console.log("Nothing to withdraw");
 
   if (balance < value) throw new Error("Insufficient Balance");
 
