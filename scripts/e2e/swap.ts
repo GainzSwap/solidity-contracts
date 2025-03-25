ad508141eb97066288a761b84d6043f588d581ea
newtmex <brightomex@gmail.com>
2025-03-25 19:39:57 +0100

    refactor swap script: extract swap logic into execSwap function for improved readability and maintainability

--- scripts/e2e/swap.ts
+++ scripts/e2e/swap.ts
@@ -19,39 +19,69 @@ export default async function swap(hre: HardhatRuntimeEnvironment, accounts: Har
   await Promise.all(
     accounts.map(async tester => {
       const { tokenIn, tokenOut } = selectTokens();
-      const swapPath = findBestPath([tokenIn, tokenOut]);
-
-      if (!swapPath || swapPath.length < 2 || !isAddressEqual(tokenIn, swapPath[0])) return;
-
       const { amount: amountIn, isNative } = await getAmount(tester, tokenIn, ethers, wnative);
-      if (amountIn <= 10_000n) return;
 
-      const token0 = await ethers.getContractAt("ERC20", tokenIn);
-      await token0.connect(tester).approve(router, 2n ** 251n);
-
-      const maxOutAmount = await views.getQuote(amountIn, swapPath);
-      const minAmountOut = (maxOutAmount * 1000n) / (BigInt(randomNumber(1, 10).toFixed()) * 1000n);
-      if (minAmountOut < 1n) return;
-
-      const args = [amountIn, minAmountOut, swapPath, tester.address, Number.MAX_SAFE_INTEGER] as const;
-      const RouterLib = require("../../verification/libs/localhost/Router.js");
-      const RouterFactory = await ethers.getContractFactory("Router", { libraries: RouterLib });
-      const referrerId = randomNumber(0, +(await router.totalUsers()).toString());
-
-      const value = isNative ? amountIn : undefined;
-      await runInErrorBoundry(
-        () =>
-          router
-            .connect(tester)
-            .registerAndSwap(
-              referrerId,
-              RouterFactory.interface.encodeFunctionData(router.swapExactTokensForTokens.name, args),
-              { value },
-            ),
-        [...slippageErrors],
-      );
-
-      console.log("Swapped", tester.address, amountIn, swapPath);
+      await execSwap({
+        swapPath: findBestPath([tokenIn, tokenOut]),
+        amountIn,
+        tokenIn,
+        router,
+        ethers,
+        tester,
+        views,
+        isNative,
+      });
     }),
   );
+}
+
+export async function execSwap({
+  swapPath,
+  amountIn,
+  tokenIn,
+  router,
+  ethers,
+  tester,
+  views,
+  isNative,
+}: {
+  swapPath: string[] | null;
+  amountIn: bigint;
+  tokenIn: string;
+  router: Router;
+  ethers: any;
+  tester: HardhatEthersSigner;
+  views: Views;
+  isNative: boolean;
+}) {
+  if (!swapPath || swapPath.length < 2 || !isAddressEqual(tokenIn, swapPath[0])) return;
+
+  if (amountIn <= 10_000n) return;
+
+  const token0 = await ethers.getContractAt("ERC20", tokenIn);
+  await token0.connect(tester).approve(router, 2n ** 251n);
+
+  const maxOutAmount = await views.getQuote(amountIn, swapPath);
+  const minAmountOut = (maxOutAmount * 1000n) / (BigInt(randomNumber(1, 10).toFixed()) * 1000n);
+  if (minAmountOut < 1n) return;
+
+  const args = [amountIn, minAmountOut, swapPath, tester.address, Number.MAX_SAFE_INTEGER] as const;
+  const RouterLib = require("../../verification/libs/localhost/Router.js");
+  const RouterFactory = await ethers.getContractFactory("Router", { libraries: RouterLib });
+  const referrerId = randomNumber(0, +(await router.totalUsers()).toString());
+
+  const value = isNative ? amountIn : undefined;
+  await runInErrorBoundry(
+    () =>
+      router
+        .connect(tester)
+        .registerAndSwap(
+          referrerId,
+          RouterFactory.interface.encodeFunctionData(router.swapExactTokensForTokens.name, args),
+          { value },
+        ),
+    [...slippageErrors],
+  );
+
+  console.log("Swapped", tester.address, amountIn, swapPath);
 }
