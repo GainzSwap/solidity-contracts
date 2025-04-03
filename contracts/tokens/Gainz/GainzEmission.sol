8b54bb53a59e554ea10a212f8dd1f05dc5c44d4e
newtmex <brightomex@gmail.com>
2025-04-03 09:00:54 +0100

    fix: adjust staking and liquidity incentive distribution percentages

--- contracts/tokens/Gainz/GainzEmission.sol
+++ contracts/tokens/Gainz/GainzEmission.sol
@@ -115,8 +115,8 @@ library Entities {
 	uint32 public constant UNITY = 100_00;
 
 	uint32 public constant TEAM = 10_00;
-	uint32 public constant STAKING = 60_00;
-	uint32 public constant LIQ_INCENTIVE = 15_00;
+	uint32 public constant STAKING = 15_00;
+	uint32 public constant LIQ_INCENTIVE = 60_00;
 	uint32 public constant GROWTH = 15_00;
 
 	struct Value {
