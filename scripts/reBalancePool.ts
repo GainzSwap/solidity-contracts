70658d9097337f1d9b51242db8af33aadbe780af
newtmex <brightomex@gmail.com>
2025-04-11 16:41:50 +0100

    refactor: streamline reBalancePool script by removing unused variables and improving token handling logic

--- scripts/reBalancePool.ts
+++ scripts/reBalancePool.ts
@@ -1,29 +1,35 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { task } from "hardhat/config";
-import { Router, Views } from "../typechain-types";
-import { formatEther, formatUnits, parseEther, parseUnits, ZeroAddress } from "ethers";
+import { Router } from "../typechain-types";
+import { formatEther, formatUnits, parseEther, parseUnits } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { getSwapTokens, isAddressEqual } from "../utilities";
+import { getSwapTokens } from "../utilities";
 
 task("reBalancePool").setAction(async (_, hre) => {
   const { ethers } = hre;
   const { deployer, newFeeTo } = await hre.getNamedAccounts();
   const router = await ethers.getContract<Router>("Router", deployer);
-  const views = await ethers.getContract<Views>("Views", deployer);
   const governance = await ethers.getContractAt("Governance", await router.getGovernance());
+  const gToken = await ethers.getContractAt("GToken", await governance.getGToken());
+  const launchPair = await ethers.getContractAt("LaunchPair", await governance.launchPair());
 
-  const [usdcEdu, gainzEdu] = await router.pairs();
+  const [pairAddr] = await router.pairs();
 
-  const gainzEduPair = await ethers.getContractAt("Pair", gainzEdu);
-  const usdcEduPair = await ethers.getContractAt("Pair", usdcEdu);
+  const pair = await ethers.getContractAt("Pair", pairAddr);
+  const token0 = await ethers.getContractAt("ERC20", await pair.token0());
+  const token1 = await ethers.getContractAt("ERC20", await pair.token1());
 
-  const gainzAddy = await governance.getGainzToken();
-  const dEDU = await router.getWrappedNativeToken();
+  const gTokenSupply = await gToken.totalSupply();
+  const totalPairLiq = await pair.totalSupply();
+  const [userGTokenAmount, userPairLiq] = (await gToken.getGTokenBalance(deployer)).reduce(
+    ([gTokenTotal, liquidity], cur) => [gTokenTotal + cur.amount, liquidity + cur.attributes.lpDetails.liquidity],
+    [0n, 0n],
+  );
 
-  const gainz = await ethers.getContractAt("Gainz", gainzAddy);
-  // await gainz.approve(router, 2n ^ 251n);
-  const usdc = "0x836d275563bAb5E93Fd6Ca62a95dB7065Da94342";
+  const wntv = await ethers.getContractAt("WNTV", await router.getWrappedNativeToken());
+  const { fundsRaised } = await launchPair.getCampaignDetails(1);
+  const totalDEDU = await wntv.totalSupply();
 
   // const { reserve0, reserve1 } = await pair.getReserves();
 
@@ -31,22 +37,32 @@ task("reBalancePool").setAction(async (_, hre) => {
   // await token1.approve(router, reserve1);
 
   const block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber());
-  let deadline = 2n ** 251n; // (block?.timestamp || 0) + 50;
+  let deadline = (block?.timestamp || 0) + 50;
   while (true) {
-    const [[gainzReserve, eduReserveGAinZ]] = await Promise.all(
-      [gainzEduPair].map(async pair => {
-        const { reserve0, reserve1 } = await pair.getReserves();
-        const token0 = await pair.token0();
+    const { reserve0, reserve1 } = await pair.getReserves();
+    const [[usdc, usdcReserve], [edu, eduReserve]] =
+      reserve0 < reserve1
+        ? [
+            [token0, reserve0],
+            [token1, reserve1],
+          ]
+        : [
+            [token1, reserve1],
+            [token0, reserve0],
+          ];
 
-        return isAddressEqual(token0, dEDU) ? [reserve1, reserve0] : [reserve0, reserve1];
-      }),
-    );
+    console.log({ usdcReserve: formatUnits(usdcReserve, 6), eduReserve: formatEther(eduReserve) });
+    const ratio = (eduReserve / usdcReserve) * 10n ** 6n;
+    console.log({ ratio: formatEther(ratio) });
 
-    // console.log({ usdcReserve: formatUnits(usdcReserve, 6), eduReserve: formatEther(eduReserve) });
-    const price = await views.getQuote(parseEther("1"), [dEDU, usdc]);
-    console.log({ price: formatUnits(price, 6) });
+    console.log({
+      tvl: formatEther(eduReserve + fundsRaised + totalDEDU),
+      liqPercent: formatUnits((userPairLiq * 100_00n) / totalPairLiq, 2),
+      eduShare: formatEther((userPairLiq * eduReserve) / totalPairLiq),
+      usdcShare: formatUnits((userPairLiq * usdcReserve) / totalPairLiq, 6),
+    });
 
-    if (price <= parseUnits("0.05", 6)) break;
+    if (ratio <= parseEther("7.300490")) break;
 
     // const eduBal = await (await ethers.getContractAt("ERC20", edu)).balanceOf(deployer);
     // eduBal > 0n &&
@@ -56,24 +72,22 @@ task("reBalancePool").setAction(async (_, hre) => {
     //     await (await ethers.getContractAt("ERC20", edu)).balanceOf(deployer),
     //   ));
 
-    const gainzBal = await gainz.balanceOf(deployer);
-    let amountIn = (gainzReserve * 4n) / 10n;
-    if (gainzBal < amountIn) amountIn = gainzBal;
+    const usdcBal = await (await ethers.getContractAt("ERC20", usdc)).balanceOf(deployer);
+    let amountIn = parseUnits("1", 6);
+    if (usdcBal < amountIn) amountIn = usdcBal;
     if (amountIn <= 0n) break;
 
-    const path = [gainzAddy, dEDU, usdc];
-    let h = await views.getAmountsOut(amountIn, path);
-    let amountOut = h.at(-1)![0];
-    amountOut *= 95n;
-    amountOut /= 100n;
+    const amountOut = (eduReserve * amountIn) / usdcReserve;
 
-    console.log({ amountIn: formatEther(amountIn), amountOut: formatUnits(amountOut, 6), path, deadline, deployer });
+    const { hash } = await router.swapExactTokensForTokens(
+      amountIn,
+      (amountOut * 998n) / 1000n,
+      [usdc, edu],
+      "0x8D0739d9D0d49aFCF8d101416cD2759Bf8922013",
+      deadline,
+    );
 
-    await gainz.approve(router, amountIn);
-
-    const { hash } = await router.swapExactTokensForTokens(amountIn, amountOut, path, deployer, deadline);
-
-    // deadline += 50;
+    deadline += 50;
 
     console.log({ hash });
   }
