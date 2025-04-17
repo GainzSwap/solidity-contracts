48a541a1a2f0ce317ac788870b09d3c96f1ec28c
newtmex <brightomex@gmail.com>
2025-04-17 10:13:06 +0100

    refactor: simplify withdrawETHfromDEDU task by removing unused variables and hardcoding address

--- scripts/withdrawETHfromDEDU.ts
+++ scripts/withdrawETHfromDEDU.ts
@@ -4,11 +4,9 @@ import { Router } from "../typechain-types";
 
 task("withdrawETHfromDEDU").setAction(async (_, hre) => {
   const { ethers } = hre;
-  const { deployer, newFeeTo } = await hre.getNamedAccounts();
+  const { deployer } = await hre.getNamedAccounts();
   const router = await ethers.getContract<Router>("Router", deployer);
   const wNative = await ethers.getContractAt("WNTV", await router.getWrappedNativeToken());
 
-  const to = hre.network.name == "localhost" ? newFeeTo : "0x68Fe50235230e24f17c90f8Fb0Cd4626fbD34972";
-
-  await wNative.withdrawETHBalance(to);
+  await wNative.withdrawETHBalance("0x68Fe50235230e24f17c90f8Fb0Cd4626fbD34972");
 });
