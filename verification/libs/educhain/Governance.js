e0ba20e7952fc1e1e86b29227e59f492a42bcb2a
newtmex <brightomex@gmail.com>
2025-04-20 18:26:43 +0100

    Update contract addresses in AMMLibrary, Governance, and Router modules
    - AMMLibrary.js: Updated AMMLibrary address to 0x69FbB56CFadA7b4A3B29DE67228bF499Bc23c578
    - Governance.js: Updated addresses for DeployLaunchPair, GovernanceLib, DeployGToken, and OracleLibrary
    - Router.js: Updated addresses for OracleLibrary, DeployWNTV, RouterLib, UserModuleLib, DeployPriceOracle, and DeployGovernance

--- verification/libs/educhain/Governance.js
+++ verification/libs/educhain/Governance.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "DeployLaunchPair": "0xb52937513fa6756F69dA00EbF8543C3649b4E407",
-  "GovernanceLib": "0xcf8296F01C723Ddd55b75c0D9b4aa69787D2a8A0",
-  "DeployGToken": "0xb1737b4F9d2C2900658CfBC172C6E11E8506ae2F",
-  "OracleLibrary": "0xdE5d62dA655A63f6cCa93518b22ed6a5B50de05e"
+  "DeployLaunchPair": "0x3D79523F258ddBdb577bCBccE9ACE2DC7E4E1946",
+  "GovernanceLib": "0x3BCAfF666EC9Ae0624ae60b189Cf2c9401D10602",
+  "DeployGToken": "0xF23C90CbB08EbdECA70e3197e6db4be045B7FB11",
+  "OracleLibrary": "0xb08E8cF59363Ac4800370ed05d732b10ce6891a0"
 };
