7e9ac18b053418e4b6d5ea12df9a3ade392bc88f
newtmex <brightomex@gmail.com>
2025-04-21 07:43:20 +0100

    feat: add Views contract integration to checkGainz script for liquidity value retrieval

--- scripts/checkGainz.ts
+++ scripts/checkGainz.ts
@@ -1,6 +1,6 @@
 import "@nomicfoundation/hardhat-toolbox";
 import { task } from "hardhat/config";
-import { Gainz, Router } from "../typechain-types";
+import { Gainz, Router, Views } from "../typechain-types";
 import { formatEther } from "ethers";
 
 task("checkGainz", "").setAction(async (_, hre) => {
@@ -8,6 +8,7 @@ task("checkGainz", "").setAction(async (_, hre) => {
   const { deployer } = await hre.getNamedAccounts();
 
   const router = await ethers.getContract<Router>("Router", deployer);
+  const views = await ethers.getContract<Views>("Views", deployer);
   const gainz = await ethers.getContract<Gainz>("Gainz", deployer);
 
   const governanceAdr = await router.getGovernance();
@@ -21,5 +22,6 @@ task("checkGainz", "").setAction(async (_, hre) => {
     stakersGainz: (+formatEther(stakersGainz)).toLocaleString(),
     stakersReserve: (+formatEther(stakersReserve)).toLocaleString(),
     rewardPerShare,
+    liq: await views.getLiquidityValue("0x597FFfA69e133Ee9b310bA13734782605C3549b7","0x32eDd6f3453f4b1F7Ad9DC4CEAF3Cff861f1080F", 33),
   });
 });
