96cdc78f1503762f2388e8f69926f7644127fbb4
newtmex <brightomex@gmail.com>
2025-04-26 10:40:42 +0100

    feat: enhance swap functionality with sequential token sending and improved error handling

--- scripts/e2e/swap.ts
+++ scripts/e2e/swap.ts
@@ -1,8 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Router, Views } from "../../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
-import { getAmount, getSwapTokens, isAddressEqual, randomNumber, runInErrorBoundry } from "../../utilities";
+import {
+  getAmount,
+  getSwapTokens,
+  isAddressEqual,
+  randomNumber,
+  runInErrorBoundry,
+  sequentialRun,
+} from "../../utilities";
 import { slippageErrors } from "./errors";
+import { sendToken } from "./transferToken";
+import { formatUnits, parseUnits } from "ethers";
 
 export default async function swap(hre: HardhatRuntimeEnvironment, accounts: HardhatEthersSigner[]) {
   console.log("Swapping");
@@ -19,18 +28,48 @@ export default async function swap(hre: HardhatRuntimeEnvironment, accounts: Har
   await Promise.all(
     accounts.map(async tester => {
       const { tokenIn, tokenOut } = selectTokens();
-      const { amount: amountIn, isNative } = await getAmount(tester, tokenIn, ethers, wnative);
+      let { amount: amountIn, isNative, decimals } = await getAmount(tester, tokenIn, ethers, wnative);
 
-      await execSwap({
-        swapPath: findBestPath([tokenIn, tokenOut]),
-        amountIn,
-        tokenIn,
-        router,
-        ethers,
-        tester,
-        views,
-        isNative,
-      });
+      const run = () =>
+        execSwap({
+          swapPath: findBestPath([tokenIn, tokenOut]),
+          amountIn,
+          tokenIn,
+          router,
+          ethers,
+          tester,
+          views,
+          isNative,
+        });
+
+      let report = await run();
+      if (report.includes("Low amount to swap")) {
+        const { newFeeTo } = await hre.getNamedAccounts();
+        const feeTo = await hre.ethers.getSigner(newFeeTo);
+
+        if (amountIn < 10_000n) {
+          amountIn = parseUnits("0.01", decimals);
+        }
+
+        await sequentialRun(feeTo, async seqAcc => {
+          let rerun = false;
+          do {
+            try {
+              await sendToken(await ethers.getContractAt("ERC20", tokenIn), amountIn, seqAcc, tester.address);
+            } catch (error) {
+              // const err = error?.toString();
+              // if (err?.includes("nonce too low") || err?.includes("nonce too high")) {
+              //   rerun = true;
+              //   console.log("Rerun sendToken", tester.address, tokenIn, amountIn);
+              // }
+            }
+          } while (rerun);
+        });
+
+        report = await run();
+      }
+
+      console.log(tester.address, ...report, formatUnits(amountIn, decimals), tokenIn, tokenOut);
     }),
   );
 }
@@ -54,16 +93,16 @@ export async function execSwap({
   views: Views;
   isNative: boolean;
 }) {
-  if (!swapPath || swapPath.length < 2 || !isAddressEqual(tokenIn, swapPath[0])) return;
+  if (!swapPath || swapPath.length < 2 || !isAddressEqual(tokenIn, swapPath[0])) return "Invalid swap path";
 
-  if (amountIn <= 10_000n) return;
+  if (amountIn <= 10_000n) return "Low amount to swap";
 
   const token0 = await ethers.getContractAt("ERC20", tokenIn);
   await token0.connect(tester).approve(router, 2n ** 251n);
 
   const maxOutAmount = await views.getQuote(amountIn, swapPath);
   const minAmountOut = (maxOutAmount * 1000n) / (BigInt(randomNumber(1, 10).toFixed()) * 1000n);
-  if (minAmountOut < 1n) return;
+  if (minAmountOut < 1n) return "Low min amount out";
 
   const args = [amountIn, minAmountOut, swapPath, tester.address, Number.MAX_SAFE_INTEGER] as const;
   const RouterLib = require("../../verification/libs/localhost/Router.js");
@@ -83,5 +122,5 @@ export async function execSwap({
     [...slippageErrors],
   );
 
-  console.log("Swapped", tester.address, amountIn, swapPath);
+  return ["Swapped", tester.address, amountIn, swapPath];
 }
